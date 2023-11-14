8b2610a71833fcbb4b3d9b254c0741b15e3bdac0
pgreaves <pgreaves@example.com>
2023-11-14 15:12:59 +0100

    Initial project structure

---
+++ src/main/java/pl/s/h/interview/Main.java
@@ -0,0 +1,10 @@
+package pl.s.h.interview;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
